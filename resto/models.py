f30e0e99695a1201c1caf83792c9f8e550dfba2b
jtomassoni <jtomassoni@gmail.com>
2020-12-11 10:20:29 -0700

    Menu, MenuSection, MenuItem

--- resto/models.py
+++ resto/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Menu(models.Model):
+    menu_title = models.CharField(max_length=100)
+    menu_description = models.TextField(blank=True, null=True)
+    active = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Menu"
+
+    def __str__(self):
+        return self.menu_title
+
+
+class MenuSection(models.Model):
+    menu = models.ForeignKey(
+        Menu, on_delete=models.CASCADE, related_name='menu_sections')
+    section_title = models.CharField(max_length=100)
+    section_description = models.TextField(blank=True, null=True)
+
+    class Meta:
+        verbose_name = "Menu Section"
+
+    def __str__(self):
+        return self.section_title
+
+
+class MenuItem(models.Model):
+    menu_section = models.ForeignKey(
+        MenuSection, on_delete=models.CASCADE, related_name='menu_items')
+    item_title = models.CharField(max_length=100)
+    item_description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    item_active = models.BooleanField()
+
+    class Meta:
+        verbose_name = "Menu Item"
+
+    def __str__(self):
+        return self.item_title
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+# from impor AbstractUser
 
 # Create your models here.
+#  class User (AbstractUser):
+#      email = models.EmailField(verbose_name='email', max_length=255, unique=True)
+#      full_name = models.CharField(max_length=60)
+#      active = models.BooleanField()
+#      role = models.CharField(max_length=20)
+#      REQUIRED_FIELDS = ['username']
+#      USERNAME_FIELD = 'email'
