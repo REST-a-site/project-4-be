42aa57b047a189ef6ca8d6e51d9c2343126de809
jtomassoni <jtomassoni@gmail.com>
2020-12-11 17:32:42 -0700

    menus now have menu_sections in them

--- resto/urls.py
+++ resto/urls.py
@@ -3,10 +3,11 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('api/menu', views.Menu.as_view()),
-    path('api/menusections', views.MenuSection.as_view()),
+    path('api/menu', views.MenuList.as_view()),
+    path('api/menu/<int:pk>', views.MenuDetail.as_view(), name="menu_detail"),
+    path('api/menu_sections', views.MenuSectionList.as_view(), name='menu_sections_list'),
     # Menu is searching for a function in views.py
-    path('api/menusections/<int:pk>', views.MenuSection.as_view(), name='menu_detail'),
+    path('api/menu_sections/<int:pk>', views.MenuSectionDetail.as_view(), name='menu_sections_detail'),
     ## Menu_sections matches the serializers path view_name
     # path('menuitem/', views.MenuItem.as_view(), name='menu_item'),
     # path('menu/<int:pk>', views.Menu.as_view(), name='section_detail')
