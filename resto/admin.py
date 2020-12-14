556f2bb1eeda562e1d8e88eff35c3251b42c8f03
jtomassoni <jtomassoni@gmail.com>
2020-12-14 10:17:17 -0700

    api object returns all selected menu items/sections, collab with Sam Goldstein

--- resto/admin.py
+++ resto/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Menu, MenuSection, MenuItem
+from .models import *
 
 # Register your models here.
-admin.site.register(Menu)
-admin.site.register(MenuSection)
+# admin.site.register(MenuSection)
 admin.site.register(MenuItem)
+admin.site.register(Menu)
--- resto/models.py
+++ resto/models.py
@@ -3,41 +3,50 @@ from django.db import models
 # Create your models here.
 
 
-class Menu(models.Model):
-    menu_title = models.CharField(max_length=100)
-    menu_description = models.TextField(blank=True, null=True)
-    active = models.BooleanField()
+# class MenuSection(models.Model):
+#     section_name = models.CharField(max_length=100)
+#     section_description = models.TextField(blank=True, null=True)
 
-    class Meta:
-        verbose_name = "Menu"
+#     class Meta:
+#         verbose_name = "Menu Section"
 
-    def __str__(self):
-        return self.menu_title
-
-
-class MenuSection(models.Model):
-    menu = models.ForeignKey(
-        Menu, on_delete=models.CASCADE, related_name='menu_sections')
-    section_title = models.CharField(max_length=100)
-    section_description = models.TextField(blank=True, null=True)
-
-    class Meta:
-        verbose_name = "Menu Section"
-
-    def __str__(self):
-        return self.section_title
+#     def __str__(self):
+#         return self.section_name
 
 
 class MenuItem(models.Model):
-    menu_section = models.ForeignKey(
-        MenuSection, on_delete=models.CASCADE, related_name='menu_items')
-    item_title = models.CharField(max_length=100)
+    CHOICES = (
+        ('Appetizers', 'Appetizers'),
+        ('Shellfish', 'Shellfish'),
+        ('Salads', 'Salads'),
+        ('Entrees', 'Entrees'),
+        ('Prime Steaks', 'Prime Steaks'),
+        ('Large Format Steak', 'Large Format Steak'),
+        ('Sides', 'Sides')
+    )
+
+    item_name = models.CharField(max_length=100)
     item_description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     item_active = models.BooleanField()
+    menu_sections = models.CharField(
+        max_length=200, choices=CHOICES, null=True)
 
     class Meta:
         verbose_name = "Menu Item"
 
     def __str__(self):
-        return self.item_title
+        return self.item_name
+
+
+class Menu(models.Model):
+    menu_name = models.CharField(max_length=100)
+    menu_description = models.TextField(blank=True, null=True)
+    active = models.BooleanField()
+    menu_item_names = models.ManyToManyField(MenuItem)
+
+    class Meta:
+        verbose_name = "Menu"
+
+    def __str__(self):
+        return self.menu_name
