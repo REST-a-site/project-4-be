d8a6c984007ff545c71ac2a04c0fa4f1a4a732e1
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2020-12-15 14:18:50 -0600

    commit

--- resto_django/settings.py
+++ resto_django/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'corsheaders',
 
     'resto',
-    # 'users',
+    'users',
 ]
 
 MIDDLEWARE = [
--- resto_django/urls.py
+++ resto_django/urls.py
@@ -5,7 +5,13 @@ from rest_framework_jwt.views import obtain_jwt_token
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('resto.urls')),
-    path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
+    path('users/', include('django.contrib.auth.urls')),
+    #^ Pointing to our users directory. django.contrib.auth is the django authentification system, it has a package called URLS that will take care of login, logout, etc... urls.
+    path('users/', include('users.urls')),
+    # ^ Here we have two URL's pointing to the same place. The order is important because django willuse the auth urls and then if it see's something else
+    # it needs to know where to go. 
+     
+     
+    # path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
 
-    # path('users/', include('users.urls')),
 ]
