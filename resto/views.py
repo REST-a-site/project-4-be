5580e2d722e4f949e88a827006b50aaaa02734a5
jtomassoni <jtomassoni@gmail.com>
2020-12-14 18:02:33 -0700

    AnonReadonly API works, collab with Sam Goldstein

--- resto/views.py
+++ resto/views.py
@@ -2,11 +2,17 @@ from rest_framework import generics
 from django.shortcuts import render
 from .serializers import *
 from .models import *
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 # Create your views here.
+
 
 class MenuList(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+
 class MenuDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
@@ -27,7 +33,15 @@ class MenuDetail(generics.RetrieveUpdateDestroyAPIView):
 def menu_list(request):
     jeff = Menu.objects.all()
     # print(jeff)
-    return render (request, 'resto/menu_list.html', {'menus': jeff})
+    return render(request, 'resto/menu_list.html', {'menus': jeff})
 
-    #on line 33 the string is the key and menus is the object
+    # on line 33 the string is the key and menus is the object
     # The actual data object is the second argument on 34, which matches the varibale for the objects.all()
+
+
+class CustomObtainAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(CustomObtainAuthToken, self).post(
+            request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({'token': token.key, 'id': token.user_id})
