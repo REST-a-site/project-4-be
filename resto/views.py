b9f03acd4ca0fc5e2aace127fa0cdc5e8850da5b
Samantha Goldstein <samanthagoldstein@Samanthas-MacBook-Pro-2.local>
2020-12-16 18:06:03 -0600

    back end up for user auth w/token login and read only access otherwise, pairing with JT

--- resto/views.py
+++ resto/views.py
@@ -2,26 +2,17 @@ from rest_framework import generics
 from django.shortcuts import render
 from .serializers import *
 from .models import *
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # Create your views here.
 
 class MenuList(generics.ListCreateAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes=[IsAuthenticatedOrReadOnly]
 
 class MenuDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+    permission_classes=[IsAuthenticatedOrReadOnly]
 
-def menu_list(request):
-    jeff = Menu.objects.all()
-    return render(request, 'resto/menu_list.html', {'menus': jeff})
-
-class CustomObtainAuthToken(ObtainAuthToken):
-    def post(self, request, *args, **kwargs):
-        response = super(CustomObtainAuthToken, self).post(
-            request, *args, **kwargs)
-        token = Token.objects.get(key=response.data['token'])
-        return Response({'token': token.key, 'id': token.user_id})
