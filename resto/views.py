f06c0e47eec7319140c1cb02df6cf4f1acaf1df8
jtomassoni <jtomassoni@gmail.com>
2020-12-17 13:55:35 -0700

    model tweaks by Sam Goldstein

--- resto/views.py
+++ resto/views.py
@@ -5,8 +5,15 @@ from .models import *
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 # Create your views here.
+
+
+class MenuList(generics.ListCreateAPIView):
+    queryset = FullMenu.objects.all()
+    serializer_class = FullMenuSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+
 class FullMenu(generics.RetrieveUpdateDestroyAPIView):
     queryset = FullMenu.objects.all()
     serializer_class = FullMenuSerializer
-    permission_classes=[IsAuthenticatedOrReadOnly]
-
+    permission_classes = [IsAuthenticatedOrReadOnly]
